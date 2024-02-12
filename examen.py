7e468413f420faa9c982e6e36a4cdcf0453ab366
= <info@josevicentecarratala.com>
2024-02-12 19:53:44 +0100

    menu

---
+++ examen.py
@@ -0,0 +1,9 @@
+def menu():
+    print("escoge una opcion")
+    print("1.-Listar registros")
+    print("2.-Buscar registros")
+    print("3.-Insertar registros")
+    print("4.-Actualizar registros")
+    print("5.-Eliminar registros")
+    print("6.-Salir")
+    
