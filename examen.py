9c02761325cfd0c50cacffec6865cb3bbab8a140
= <info@josevicentecarratala.com>
2024-02-12 19:54:51 +0100

    opciones

--- examen.py
+++ examen.py
@@ -7,3 +7,15 @@ def menu():
     print("5.-Eliminar registros")
     print("6.-Salir")
     opcion = input("Opción escogida:")
+    if opcion == "1":
+        print("Listamos registros");
+    elif opcion == "2":
+        print("Buscamos registros")
+    elif opcion == "3":
+        print("Insertamos registros")
+    elif opcion == "4":
+        print("Actualizamos registros")
+    elif opcion == "5":
+        print("Eliminamos registros")
+    elif opcion == "6":
+        print("Salimos")
